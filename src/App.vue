b0a84f968422c508a610e61572411ee60763c321
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-04-23 11:01:36 -0300

    Create Vue application

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="search-container">
+    <input
+      v-model.trim="term"
+      type="search"
+      class="search-field"
+    />
+  </section>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        term: '',
+      };
+    },
+  };
+</script>
+
+<style lang="stylus" src="@/assets/styles/base.styl"></style>
+
+<style lang="stylus">
+  .search-field
+    width: 100%
+    height: 60px
+    padding-left: (@height / 2)
+    padding-right: @padding-left
+    border: 1px solid #000
+    border-radius: @height
+</style>
